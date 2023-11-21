6059af060a8e5c4f995b71301934a6847d8f005c
Rodrigo <rodrigo.gomezr21@gmail.com>
2023-11-20 22:31:40 -0300

    Update api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-99ZQEEP08K");
 
+ReactGA.send({
+  hitType:"pageview",
+  page: window.location.pathname,
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
